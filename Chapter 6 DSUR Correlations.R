25bcc0df50aa7031a0a05bc48e6e665cc08da1f6
Lawrence Chen <lawrence009@outlook.com>
2015-06-29 10:46:23 -0700

    chapter 7 regression

--- Chapter 6 DSUR Correlations.R
+++ Chapter 6 DSUR Correlations.R
@@ -177,7 +177,6 @@ pc^2
 pcor.test(pc, 1, 103)
 
 #----Differences between independent rs----
-
 zdifference<-function(r1, r2, n1, n2) {
     zd <- (atanh(r1)-atanh(r2))/sqrt(1/(n1-3)+1/(n2-3))
 	p <- 1 - pnorm(abs(zd))
@@ -187,8 +186,7 @@ zdifference<-function(r1, r2, n1, n2) {
 
 zdifference(-0.506, -0.381, 52, 51)
 
-#-------Differences between dependent rs-----
-
+#----Differences between dependent rs----
 tdifference<-function(rxy, rxz, rzy, n) {
     df<-n-3
 	td<-(rxy-rzy)*sqrt((df*(1 + rxz))/(2*(1-rxy^2-rxz^2-rzy^2+(2*rxy*rxz*rzy))))
